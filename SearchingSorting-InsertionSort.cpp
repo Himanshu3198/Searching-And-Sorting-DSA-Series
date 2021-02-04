538ea1511fde6206ffb7b3384838a240f7bb7457
HIMANSHU SHARMA <54854216+Himanshu3198@users.noreply.github.com>
2021-02-04 20:54:45 +0530

    Add files via upload

---
+++ SearchingSorting-InsertionSort.cpp
@@ -0,0 +1,37 @@
+// Insertion sort implementation using cpp
+// time complexity O(n*2)
+#include <bits/stdc++.h>
+using namespace std;
+
+void SelectionSort(int arr[], int n)
+{
+    for (int i = 1; i < n; i++)
+    {
+        int key = arr[i];
+
+            int j = i - 1;
+        while ((j >= 0) && key < arr[j])
+        {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = key;
+    }
+}
+
+  void printArray(int arr[],int n){
+
+      for(int i=0;i<n;i++){
+          cout<<arr[i]<<" ";
+      }
+      cout<<"\n";
+  }
+ 
+int main()
+{
+    int arr[] = {12, 13, 3, 2, 8};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    SelectionSort(arr, n);
+    printArray(arr,n);
+    return 0;
+}
