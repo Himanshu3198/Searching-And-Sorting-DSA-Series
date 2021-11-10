b1d23945c4ce2b3e5ae368b707be3008ae230282
HIMANSHU SHARMA <54854216+Himanshu3198@users.noreply.github.com>
2021-11-10 13:58:05 +0530

    Create Binary-Search-275. H-Index II.cpp

---
+++ Binary-Search-275. H-Index II.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int hIndex(vector<int>& nums) {
+        
+        
+          int low=0,high=nums.size()-1;
+             int n=nums.size();
+          
+            while(low<=high){
+                
+                
+                int mid=low+(high-low)/2;
+                
+                if(nums[mid]==(n-mid)){
+                    
+                    return nums[mid];
+                }
+                
+                else if(nums[mid]>(n-mid)){
+                    
+                    high=mid-1;
+                }
+                else{
+                    low=mid+1;
+                }
+            }
+        
+            return (n-low);
+    }
+};
