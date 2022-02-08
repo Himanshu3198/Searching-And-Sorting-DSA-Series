67d3e42fc385483172cd7e398787371fa5c26090
HIMANSHU SHARMA <54854216+Himanshu3198@users.noreply.github.com>
2022-02-08 23:54:31 +0530

    Update Binary-Search-Koko-eating-bananas.cpp

--- Binary-Search-Koko-eating-bananas.cpp
+++ Binary-Search-Koko-eating-bananas.cpp
@@ -1,55 +1,58 @@
 class Solution {
 public:
     
-    /* problem in  this we have to choose min no of banana's eat per hours so that all 
-    banana's eaten within given deadline hour
-    As we knew the answer range will be between the 1 to max no of bananas in given piles
-    So we weill check for all the  possible minimum value within this range .if we solve it iteratively then time compexity 
-    will be (max_val*size of piles) we can improve it using binary search*/
+//      we need to find minimum speed at which koko can finish all bananas within h hours
     
-    bool isValid(int mid,vector<int>&piles,int h){
+    bool valid(int mid,int h,vector<int>&nums){
         
         
-        int count=0;
+        int time=0;
         
-        for(int i=0;i<piles.size();i++){
+    for(int i=0;i<nums.size();i++){
+        
+        
+        if(nums[i]%mid){
             
-            if(piles[i]%mid){
-                
-                count+=(piles[i]/mid)+1;
-            }
-            else{
-                count+=(piles[i]/mid);
-            }
+            time+=(nums[i]/mid +1);
+        }else{
+            
+            time+=(nums[i]/mid);
         }
-        return count<=h;
         
+   
+    }
+            return time<=h;
     }
         
-    
-    int minEatingSpeed(vector<int>& piles, int h) {
-     
         
-          int low=1,high=*max_element(piles.begin(),piles.end());
+    int minEatingSpeed(vector<int>& piles, int h) {
+        
+        
+        
+        int low=1;
+        
+        int high=*max_element(piles.begin(),piles.end());
         
         
         int ans=high;
-        while(low<=high){
+        while(low<high){
             
             
             int mid=(low+high)/2;
             
             
-            if(isValid(mid,piles,h)==1){
-                 ans=min(ans,mid);
-                high=mid-1;
-            }
-            else{
+            if(valid(mid,h,piles)){
+                
+                high=mid;
+                ans=mid;
+            }else{
+                
                 low=mid+1;
             }
+              
+            
         }
         
         return ans;
-  
     }
 };
