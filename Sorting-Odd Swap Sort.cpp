cbddcc18b4b84a6b007662b3004e415bc2687570
HIMANSHU SHARMA <54854216+Himanshu3198@users.noreply.github.com>
2022-02-15 16:10:33 +0530

    Update Sorting-Odd Swap Sort.cpp

--- Sorting-Odd Swap Sort.cpp
+++ Sorting-Odd Swap Sort.cpp
@@ -4,91 +4,91 @@
  https://codeforces.com/contest/1638/problem/B
 
 */
-#include <bits/stdc++.h>
-#define ll long long int
-#define fr(i, n) for (int i = 0; i < (n); i++)
-#define rep(i, a, b) for (int i = (a); i < (b); i++)
-#define FORR(i, n) for (int i = (n); i >= 0; i--)
-#define fst first
-#define snd second
-#define E "\n"
-#define pb push_backCF
-#define mp make_pair
-#define all(x) (x).begin(), (x).end()
-#define MOD 1000000007LL
-#define MAX(a, b) ((a) > (b) ? (a) : (b))
-#define MIN(a, b) ((a) < (b) ? (a) : (b))
-#define ABS(x) ((x) < 0 ? -(x) : (x))
-#define setbits(x) __builtin_popcount(x)
-#define fastio()                      \
-    ios_base::sync_with_stdio(false); \
-    cin.tie(NULL);
+#include     <bits/stdc++.h>
+#define      ll long long int
+#define      fr(i, n) for (int i = 0; i < (n); i++)
+#define      rep(i, a, b) for (int i = (a); i < (b); i++)
+#define      FORR(i, n) for (int i = (n); i >= 0; i--)
+#define      fst first
+#define      snd second
+#define      E "\n"
+#define      pb push_backCF
+#define      mp make_pair
+#define      all(x) (x).begin(), (x).end()
+#define      MOD 1000000007LL
+#define      MAX(a, b) ((a) > (b) ? (a) : (b))
+#define      MIN(a, b) ((a) < (b) ? (a) : (b))
+#define      ABS(x) ((x) < 0 ? -(x) : (x))
+#define      setbits(x) __builtin_popcount(x)
+#define      vi vector<int>
+#define      vl vector<long long int>
+#define      vvi vector<vector<int>>
+#define      vvl vector<vector<long long int>>
+#define      INF 1e9
+#define      _Y puts("Yes")
+#define      _N puts("No")
+#define      showArr(v) for(auto it:v) cout<<it<<" ";
+#define      BhaagBC()                      \
+             ios_base::sync_with_stdio(false); \
+             cin.tie(NULL);
+
 using namespace std;
 
-void solve()
+
+
+void himanshu3198()
 {
    
    
   ll n;
   cin>>n;
 
-  vector<ll>a(n);
+   vi a(n);
 
 
   fr(i,n) cin>>a[i];
 
-
-    bool flag=true;
-    bool endgame=false;
-  for(ll len=1;len<=n;len++){
- 
-   
-       flag=true;
-    for(int i=0;i<n;i++){
-               
-            
-        if(a[i]>a[i+1]){
-             flag=false;
-            ll odd=(a[i]+a[i+1]);
-            if(a[i]>a[i+1] and odd%2==0){
-                endgame=true;
-                break;
-            }
-          
-            if(odd%2==1){
-                swap(a[i],a[i+1]);
-            }
-        }
-    }
-    if(flag==true){
-        break;
-    }
+ int  lastEven=0,lastOdd=1;
   
-    
-    if(endgame){
-        break;
-    }
-  }
+//    showArr(a);
 
-  if(flag and !endgame){
-      puts("Yes");
-  }else{
-      puts("No");
+  fr(i,n){
+
+      if(a[i]&1){
+   
+       if(a[i]<lastOdd){
+           _N;
+           return;
+       }
+       lastOdd=a[i];
+       
+      }else{
+ 
+         if(a[i]<lastEven){
+            _N;
+             return;
+         }
+         lastEven=a[i];
+      }
   }
+  
+       _Y;  
+    
+
 
 }
 
 
 int main()
 {
-    fastio();
+    BhaagBC();
     int t = 1;
     cin >> t;
 
     while (t--)
     {
 
-        solve();
+        himanshu3198();
     }
 
     return 0;
