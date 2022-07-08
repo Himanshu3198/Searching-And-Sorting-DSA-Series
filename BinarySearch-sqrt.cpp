eff35f32ca11d8ace4823bbe9acc124316c95908
HIMANSHU SHARMA <54854216+Himanshu3198@users.noreply.github.com>
2022-07-08 23:14:03 +0530

    Update BinarySearch-sqrt.cpp

--- BinarySearch-sqrt.cpp
+++ BinarySearch-sqrt.cpp
@@ -1,18 +1,20 @@
-// 69. Sqrt(x)
-
 class Solution {
 public:
     int mySqrt(int x) {
-        int low = 0,  high = x, mid;
-        if(x<2) return x; 
-        while(low<high)
-        {
-            mid = (low + high)/2;
-            if(x/mid >= mid) low = mid+1;
-            else high = mid;
+        
+        int low=0,high=x;
+        
+        while(low<=high){
+            
+            long long mid=(low+high)/2;
+            
+            if(mid*mid==x) return mid;
+            
+            if(mid*mid>x){
+                
+                high=mid-1;
+            }else low=mid+1;
         }
-        return high-1;
-        
-        
+        return low-1;
     }
 };
