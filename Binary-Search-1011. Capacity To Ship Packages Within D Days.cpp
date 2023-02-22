616e7d12d845cfc5f2d32dd0cedbf6328d2850bf
HIMANSHU SHARMA <54854216+Himanshu3198@users.noreply.github.com>
2023-02-23 00:16:59 +0530

    Update Binary-Search-1011. Capacity To Ship Packages Within D Days.cpp

--- Binary-Search-1011. Capacity To Ship Packages Within D Days.cpp
+++ Binary-Search-1011. Capacity To Ship Packages Within D Days.cpp
@@ -1,62 +1,29 @@
-1011. Capacity To Ship Packages Within D Days
 class Solution {
 public:
-    
-    bool isSafe(int mid,vector<int>&weights,int days){
-        
-        int count=0;
-        int sum=0;
+    bool ok(vector<int>&weights,int days,int w){
+        int count=0,sum=0;
         for(int i=0;i<weights.size();i++){
-            
-            
-            if((sum+weights[i])<=mid){
+            if(count>days || weights[i]>w) return false;
             sum+=weights[i];
-            }
-            else{
-                sum=0;
+            if(sum>w){
+                sum=weights[i];
                 count++;
-                i--;
             }
-            
         }
-            
-        
-            
-             return count+1<=days;
-        
+        count++;
+        return count<=days;
     }
     int shipWithinDays(vector<int>& weights, int days) {
-     
-            
-        int low=*max_element(weights.begin(),weights.end());
-        
-     
-      int high=  accumulate(weights.begin(),weights.end(),0);
-        cout<<high;
-        
-        int ans=high;
+        int tot=accumulate(weights.begin(),weights.end(),0);
+        int low=1,high=tot,ans=high;
         while(low<=high){
-            
-            
             int mid=(low+high)/2;
-            
-            
-            if(isSafe(mid,weights,days)==1){
-                
-                ans=min(ans,mid);
-                
+            if(ok(weights,days,mid)){
+                ans=mid;
                 high=mid-1;
-            }
-            else{
-                low=mid+1;
-            }
-            
-            
+            }else low=mid+1;  
         }
-        
         return ans;
-        
-    
         
     }
 };
